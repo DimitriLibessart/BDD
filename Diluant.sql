6d91b201484cfc4800f472a7312713a15a40ef94
Balentins <valentin.naessens@viacesi.fr>
2017-03-31 16:12:01 +0200

    correction

--- Diluant.sql
+++ Diluant.sql
@@ -1,6 +1,15 @@
-ISSERT INTO Diluant
+INSERT INTO Diluant
 VALUES
 (1, 'Bière'),
 (2, 'Eau'),
 (3, 'Jus de fruit'),
 (4, 'Vin');
+
+
+INSERT INTO Unite
+VALUES
+(1;Pincee),
+(2;Soufle),
+(3;Nuage),
+(4;Poignee),
+(5;Once);
