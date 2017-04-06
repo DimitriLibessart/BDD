4876487a6877361bc19180784aa5b8720b3e9808
Balentins <valentin.naessens@viacesi.fr>
2017-04-06 16:04:32 +0200

    Add files via upload

--- Suppression_recette.sql
+++ Suppression_recette.sql
@@ -3,21 +3,25 @@ CREATE PROCEDURE Suppression_recette()
 BEGIN
 
 SET @ID = (SELECT ID_Potion FROM  recette WHERE Statue = '0');
-
-DELETE FROM recette
-WHERE ID_Potion = @ID;
+SET @ID2 = ID;
+SET @ID3 = ID2;
+SET @ID4 = ID3;
+SET @ID5 = ID4;
 
 DELETE FROM contient 
-WHERE ID_Potion = @ID;
-
-DELETE FROM compose 
-WHERE ID_Onguent = @ID;
-
-DELETE FROM onguent
-WHERE ID_Onguent = @ID;
+WHERE ID_Potion = @ID OR D_Potion = @ID2 OR D_Potion = @ID3 OR D_Potion = @ID4 OR D_Potion = @ID5;
 
 DELETE FROM potion 
-WHERE ID_Potion = @ID;
+WHERE ID_Potion = @ID OR D_Potion = @ID2 OR D_Potion = @ID3 OR D_Potion = @ID4 OR D_Potion = @ID5;
+
+DELETE FROM compose 
+WHERE ID_Potion = @ID OR D_Potion = @ID2 OR D_Potion = @ID3 OR D_Potion = @ID4 OR D_Potion = @ID5;
+
+DELETE FROM onguent
+WHERE ID_Potion = @ID OR D_Potion = @ID2 OR D_Potion = @ID3 OR D_Potion = @ID4 OR D_Potion = @ID5;
+
+DELETE FROM recette
+WHERE ID_Potion = @ID OR D_Potion = @ID2 OR D_Potion = @ID3 OR D_Potion = @ID4 OR D_Potion = @ID5;
 
 END |
 DELIMITER ;
