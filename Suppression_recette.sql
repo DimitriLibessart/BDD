fe7449d110bbaa1a07d43ae579bafbeddb32f8aa
Balentins <valentin.naessens@viacesi.fr>
2017-04-06 15:57:31 +0200

    Add files via upload

--- Suppression_recette.sql
+++ Suppression_recette.sql
@@ -2,7 +2,7 @@ DELIMITER |
 CREATE PROCEDURE Suppression_recette()
 BEGIN
 
-SET @ID = (SELECT ID_Potion FROM  recette WHERE Statut = '0');
+SET @ID = (SELECT ID_Potion FROM  recette WHERE Statue = '0');
 
 DELETE FROM recette
 WHERE ID_Potion = @ID;
@@ -11,7 +11,7 @@ DELETE FROM contient
 WHERE ID_Potion = @ID;
 
 DELETE FROM compose 
-WHERE ID_Potion = @ID;
+WHERE ID_Onguent = @ID;
 
 DELETE FROM onguent
 WHERE ID_Onguent = @ID;
