19780f7e955d089af9183d8d59050d1e2651dc76
Balentins <valentin.naessens@viacesi.fr>
2017-04-06 16:14:27 +0200

    Add files via upload

--- Suppression_recette.sql
+++ Suppression_recette.sql
@@ -8,17 +8,23 @@ SET @ID = (SELECT ID_Potion FROM  recette WHERE Statue = '0');
 DELETE FROM contient 
 WHERE ID_Potion = @ID ;
 
+SET @ID = (SELECT ID_Potion FROM  recette WHERE Statue = '0');
+
 DELETE FROM potion 
-WHERE ID_Potion = @ID 
+WHERE ID_Potion = @ID ;
+
+SET @ID = (SELECT ID_Potion FROM  recette WHERE Statue = '0');
 
 DELETE FROM compose 
-WHERE ID_Potion = @ID ;
+WHERE ID_Onguent = @ID ;
+
+SET @ID = (SELECT ID_Potion FROM  recette WHERE Statue = '0');
 
 DELETE FROM onguent
-WHERE ID_Potion = @ID ;
+WHERE ID_Onguent = @ID ;
 
 DELETE FROM recette
-WHERE ID_Potion = @ID ;
+WHERE Statue = '0' ;
 
 END |
 DELIMITER ;
